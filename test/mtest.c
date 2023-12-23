3d2373c123ba8cb64b0ee1f72fe97ae19876acd8
sergio8735 <127796005+sergio8735@users.noreply.github.com>
2023-12-23 23:57:00 +0100

    Added profiling to the main test file. Plans to make it into a more formal feature

--- test/mtest.c
+++ test/mtest.c
@@ -14,6 +14,8 @@
  *****************************************************************************/
 
 
+#include <sys/time.h>
+
 #include "../include/camel.h"
 
 
@@ -28,6 +30,7 @@ void cml_register_all_tests(CML_Test *registry, u32 *count) {
 }
 
 
+
 int main() {
     u32 count = 0;
     u32 expectedCount = 200;
@@ -37,9 +40,51 @@ int main() {
     // Better create the registry on the stack, so we 
     // don't have to free it.
 
-    cml_register_all_tests(&registry, &count);
+    cml_register_all_tests(registry, &count);
 
-    cml_run_tests(&registry, count);
-    
+    cml_run_tests(registry, count);
+
+
+    int profiling = 1;
+    if (profiling) {
+        // Time profiling
+        u32 iterations = 1000000;
+        u32 outIterations = 1000;
+        printf("\n\nTime profiling:\n");
+        printf("Function being profiled: cml_matrix4x4_mult\n");
+        printf("Iterations: %d\n", iterations);
+
+        struct timeval start, end;
+        CML_Matrix4x4 A = {
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0, 7.0
+        };
+        CML_Matrix4x4 B = {
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0, 7.0
+        };
+        CML_Matrix4x4 C;
+        double totalElapsed = 0.0;
+        double elapsed = 0.0;
+        for (u32 i = 0; i < outIterations; i++) {
+            gettimeofday(&start, NULL);
+            for (u32 i = 0; i < iterations; i++) {
+                cml_matrix4x4_mult(A, B, C);
+            }
+            gettimeofday(&end, NULL);
+            elapsed = (end.tv_sec - start.tv_sec) + ((end.tv_usec - start.tv_usec) / 1000000.0);
+            totalElapsed += elapsed;
+        }
+        elapsed = totalElapsed / outIterations;
+        printf("Elapsed time: %fs\n", totalElapsed);
+        printf("Average elapsed time per %u iterations: %fs\n", iterations, elapsed);
+        printf("Average elapsed time per iteration: %.12fs\n", elapsed / iterations);
+        printf("Average iterations per second: %f\n", 1.0 / (elapsed / iterations));
+    }
+
     return 0;
 }
